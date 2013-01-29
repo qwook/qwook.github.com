c615593a70616e4faaa980c2dab4562a7e628c89
Henry <henrytran@Henrys-MacBook-Pro.local>
2013-01-28 17:37:24 -0800

    projects

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 (function() {
-  var ele, fakejax, loading, spin, xmlhttp, _i, _len, _ref;
+  var fakejax, loading, spin, xmlhttp;
 
   loading = false;
 
@@ -24,12 +24,16 @@
         if (xmlhttp.status === 200) {
           arr = xmlhttp.responseText.match(fakejax);
           document.getElementById("content").innerHTML = arr[0];
+          window.loadlinks();
         } else if (xmlhttp.status === 404) {
           document.getElementById("content").innerHTML = "<h1><b>404</b> page not found.</h1>";
         }
         document.getElementById("content").style.opacity = "1";
+        window.scrollTo(window.scrollX || 0, 0);
         loading = false;
-        return spin.parentNode.removeChild(spin);
+        if (spin !== null && spin.parentNode !== null) {
+          return spin.parentNode.removeChild(spin);
+        }
       }, 150);
     }
   };
@@ -37,7 +41,9 @@
   xmlhttp.ontimeout = function() {
     document.getElementById("content").innerHTML = "<p>Request timed out, I'm sorry :(</p>";
     loading = false;
-    return spin.parentNode.removeChild(spin);
+    if (spin !== null && spin.parentNode !== null) {
+      return spin.parentNode.removeChild(spin);
+    }
   };
 
   window.onpopstate = function(obj) {
@@ -49,28 +55,35 @@
     }
   };
 
-  _ref = document.getElementsByClassName('link');
-  for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-    ele = _ref[_i];
-    ele.onclick = function() {
-      var img;
-      if (!loading) {
-        spin = document.createElement('div');
-        spin.setAttribute('class', 'spin');
-        img = document.createElement('img');
-        img.setAttribute('src', './load.png');
-        spin.appendChild(img);
-        this.appendChild(spin);
-        loading = true;
-        document.getElementById("content").style.opacity = "0.25";
-        xmlhttp.open("GET", this.getAttribute("href"), true);
-        xmlhttp.send();
-        history.pushState({
-          url: this.getAttribute("href")
-        }, "", this.getAttribute("href"));
-      }
-      return false;
-    };
-  }
+  window.loadlinks = function() {
+    var ele, _i, _len, _ref, _results;
+    _ref = document.getElementsByClassName('link');
+    _results = [];
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      ele = _ref[_i];
+      _results.push(ele.onclick = function() {
+        var img;
+        if (!loading) {
+          spin = document.createElement('div');
+          spin.setAttribute('class', 'spin');
+          img = document.createElement('img');
+          img.setAttribute('src', './load.png');
+          spin.appendChild(img);
+          this.appendChild(spin);
+          loading = true;
+          document.getElementById("content").style.opacity = "0.25";
+          xmlhttp.open("GET", this.getAttribute("href"), true);
+          xmlhttp.send();
+          history.pushState({
+            url: this.getAttribute("href")
+          }, "", this.getAttribute("href"));
+        }
+        return false;
+      });
+    }
+    return _results;
+  };
+
+  window.loadlinks();
 
 }).call(this);
