1252187993d91c1fa5da9bc9e0cd7cd5e697e98c
Henry Tran <henryquoctran@gmail.com>
2024-08-24 01:44:52 -0700

    move to docs

--- webpack.common.js
+++ webpack.common.js
@@ -34,7 +34,7 @@ module.exports = {
   },
   output: {
     filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "site"),
+    path: path.resolve(__dirname, "docs"),
   },
   // optimization: {
   //   splitChunks: {
