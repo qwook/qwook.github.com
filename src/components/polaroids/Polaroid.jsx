c43b343d45d7a2254ca2fbc6d22f5ce0d6567f18
Henry Tran <henryquoctran@gmail.com>
2024-10-17 00:51:54 -0700

    freeze on tap

--- src/components/polaroids/Polaroid.jsx
+++ src/components/polaroids/Polaroid.jsx
@@ -34,41 +34,54 @@ export default function Polaroid({ idx }) {
     backs[Math.floor(random * 100000) % backs.length]
   );
 
+  const falling = useRef(false);
   const alreadyReset = useRef(false);
 
   const front = useFrame((state, delta) => {
-    // meshRef.current.rotation.x += delta * 0.5;
-    // meshRef.current.rotation.y += delta * 2.0;
     const now = performance.now() * 0.5 + 100000;
-    // console.log(now);
-    meshRef.current.rotation.y =
-      Math.cos((idx * 1.0 - now / 2000 + 10) % (Math.PI * 2)) * Math.PI +
-      Math.PI +
-      Math.cos(random * 10) / 4;
-    meshRef.current.rotation.z =
-      ((idx * 0.3 - now / 2000) % (Math.PI * 4)) + Math.PI / 2;
-    meshRef.current.position.x = Math.sin(meshRef.current.rotation.y) * 2.5;
-    meshRef.current.position.z =
-      Math.cos(meshRef.current.rotation.y) *
-        (0.5 + Math.abs(Math.cos(idx * 1.0 - now / 2000 - 10)) * 2) -
-      1.5;
-    meshRef.current.position.y = ((idx * 1.0 - now / 1000) % 14) + 7;
+    if (falling.current) {
+      meshRef.current.position.y = ((idx * 1.0 - now / 1000) % 15) + 7;
+    } else {
+      // meshRef.current.rotation.x += delta * 0.5;
+      // meshRef.current.rotation.y += delta * 2.0;
+      // console.log(now);
+      meshRef.current.rotation.y =
+        Math.cos((idx * 1.0 - now / 2000 + 10) % (Math.PI * 2)) * Math.PI +
+        Math.PI +
+        Math.cos(random * 10) / 4;
+      meshRef.current.rotation.z =
+        ((idx * 0.3 - now / 2000) % (Math.PI * 4)) + Math.PI / 2;
+      meshRef.current.position.x = Math.sin(meshRef.current.rotation.y) * 2.5;
+      meshRef.current.position.z =
+        Math.cos(meshRef.current.rotation.y) *
+          (0.5 + Math.abs(Math.cos(idx * 1.0 - now / 2000 - 10)) * 2) -
+        1.5;
+      meshRef.current.position.y = ((idx * 1.0 - now / 1000) % 15) + 7;
+    }
 
     if (meshRef.current.position.y > 0) {
       alreadyReset.current = false;
     }
 
-    if (meshRef.current.position.y < -8) {
+    if (meshRef.current.position.y < -7) {
       if (!alreadyReset.current) {
         alreadyReset.current = true;
         setRandom(incrementGlob());
+        falling.current = false;
       }
     }
   });
 
   return (
     <>
-      <object3D ref={meshRef} scale={[1, 1003 / 630, 1]}>
+      <object3D
+        ref={meshRef}
+        scale={[1, 1003 / 630, 1]}
+        onClick={(e) => {
+          falling.current = true;
+          console.log("Falling!");
+        }}
+      >
         <mesh>
           <planeGeometry args={[2, 2]} />
           <meshPhongMaterial
