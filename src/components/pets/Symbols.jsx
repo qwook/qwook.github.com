f13b519b6e09cff5efb49313fcb808c760330944
Henry Tran <henryquoctran@gmail.com>
2024-10-18 17:28:45 -0700

    Add new symbol images and hover descriptions

--- src/components/pets/Symbols.jsx
+++ src/components/pets/Symbols.jsx
@@ -1,12 +1,48 @@
 const Symbols = {
   Health: function () {
-    return <span className="symbol">&hearts;</span>;
+    return (
+      <span
+        className="symbol"
+        style={{
+          display: "inline-block",
+          verticalAlign: "top",
+          width: "1em",
+          height: "1em",
+          backgroundImage: `url("${require("./images/icons/heart.png")}")`,
+          backgroundSize: "cover",
+        }}
+      ></span>
+    );
   },
   Happiness: function () {
-    return <span className="symbol">&#9786;</span>;
+    return (
+      <span
+        className="symbol"
+        style={{
+          display: "inline-block",
+          verticalAlign: "top",
+          width: "1em",
+          height: "1em",
+          backgroundImage: `url("${require("./images/icons/smiley.png")}")`,
+          backgroundSize: "cover",
+        }}
+      ></span>
+    );
   },
   Points: function () {
-    return <span className="symbol">✿</span>;
+    return (
+      <span
+        className="symbol"
+        style={{
+          display: "inline-block",
+          verticalAlign: "top",
+          width: "1em",
+          height: "1em",
+          backgroundImage: `url("${require("./images/icons/flower.png")}")`,
+          backgroundSize: "cover",
+        }}
+      ></span>
+    );
   },
 };
 
