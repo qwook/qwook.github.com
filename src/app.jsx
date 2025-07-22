9ad20dc0b947268cbe1204f4989b1f53a387deba
Henry Tran <henryquoctran@gmail.com>
2025-07-22 23:22:09 +0700

    Oops blank page

--- src/app.jsx
+++ src/app.jsx
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom/client";
 
 export function createPage(App, options = {}) {
   // Render the React component into the DOM
-  // document.addEventListener("DOMContentLoaded", () => {
-  //   const root = ReactDOM.createRoot(document.getElementById("root"));
-  //   root.render(
-  //     <Page {...options}>
-  //       <App />
-  //     </Page>
-  //   );
-  // });
+  document.addEventListener("DOMContentLoaded", () => {
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    root.render(
+      <Page {...options}>
+        <App />
+      </Page>
+    );
+  });
 }
