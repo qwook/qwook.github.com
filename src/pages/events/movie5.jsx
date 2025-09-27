1a9af8b8e41c9972bfccc1a485a299247de158ad
Henry Tran <henryquoctran@gmail.com>
2025-09-27 15:58:51 +0700

    fix issue with page loading every other page

--- src/pages/events/movie5.jsx
+++ src/pages/events/movie5.jsx
@@ -13,52 +13,58 @@ headTags({
   image: movieDetails.image,
 });
 
-import { createPage } from "../../app";
-import { Event } from "../../components/events/event";
-import { Genre } from "../../components/events/genre";
+// The things I do to get metatags to be exported.....
+(async () => {
+  if (global.lol) return;
+  global.lol = true;
 
-const TITLE = "After Life (1998)";
+  const { createPage } = await import("../../app");
+  const { Event } = await import("../../components/events/event");
+  const { Genre } = await import("../../components/events/genre");
 
-export default function EventPage() {
-  return (
-    <Event
-      title={TITLE}
-      start="July 28, 2025 7:00 PM"
-      duration={[2, "hour"]}
-      host={{
-        name: "Henry",
-        url: "https://instagram.com/nohurryhen",
-      }}
-      location={{
-        address: "235/1 Đ. Nam Kỳ Khởi Nghĩa, Phường 14, Quận 3",
-        url: "https://maps.app.goo.gl/cRmTBGFojgXYbwNP9",
-      }}
-    >
-      <img src={require("./images/afterlife.gif")} />
-      <p>Runtime: 1 hours 59 minutes</p>
-      <p>
-        Genre: <Genre list={["Tragicomedy", "Fantasy", "Drama"]} />
-      </p>
-      <p>🇺🇸 ENSUB + 🇻🇳 VIETSUB</p>
-      <p>
-        After souls leave their bodies on Earth, they go through a process
-        before entering their personal heaven, a single special memory they want
-        to live for eternity.
-      </p>
-      <p>What is the one memory you would take with you?</p>
-      <p>bring snacks and beer if you want</p>
-      <p>
-        <strong>
-          The above address is a restaurant near my house, please message me for
-          my real address!
-        </strong>
-      </p>
-      <b>My room is small, please ask me before bringing friends.</b>
-    </Event>
-  );
-}
+  const TITLE = "After Life (1998)";
 
-createPage(EventPage, {
-  showPets: false,
-  title: TITLE,
-});
+  function EventPage() {
+    return (
+      <Event
+        title={TITLE}
+        start="July 28, 2025 7:00 PM"
+        duration={[2, "hour"]}
+        host={{
+          name: "Henry",
+          url: "https://instagram.com/nohurryhen",
+        }}
+        location={{
+          address: "235/1 Đ. Nam Kỳ Khởi Nghĩa, Phường 14, Quận 3",
+          url: "https://maps.app.goo.gl/cRmTBGFojgXYbwNP9",
+        }}
+      >
+        <img src={require("./images/afterlife.gif")} />
+        <p>Runtime: 1 hours 59 minutes</p>
+        <p>
+          Genre: <Genre list={["Tragicomedy", "Fantasy", "Drama"]} />
+        </p>
+        <p>🇺🇸 ENSUB + 🇻🇳 VIETSUB</p>
+        <p>
+          After souls leave their bodies on Earth, they go through a process
+          before entering their personal heaven, a single special memory they
+          want to live for eternity.
+        </p>
+        <p>What is the one memory you would take with you?</p>
+        <p>bring snacks and beer if you want</p>
+        <p>
+          <strong>
+            The above address is a restaurant near my house, please message me
+            for my real address!
+          </strong>
+        </p>
+        <b>My room is small, please ask me before bringing friends.</b>
+      </Event>
+    );
+  }
+
+  createPage(EventPage, {
+    showPets: false,
+    title: TITLE,
+  });
+})();
--- src/pages/events/movie6.jsx
+++ src/pages/events/movie6.jsx
@@ -12,58 +12,65 @@ headTags({
   image: movieDetails.image,
 });
 
-import { createPage } from "../../app";
-import { Event } from "../../components/events/event";
-import { Genre } from "../../components/events/genre";
+// The things I do to get metatags to be exported.....
+(async () => {
+  if (global.lol) return;
+  global.lol = true;
 
-const TITLE = "Before Sunset (2004)";
+  const { createPage } = await import("../../app");
+  const { Event } = await import("../../components/events/event");
+  const { Genre } = await import("../../components/events/genre");
 
-export default function EventPage() {
-  return (
-    <Event
-      title={TITLE}
-      start="August 26, 2025 7:30 PM"
-      duration={[2, "hour"]}
-      host={{
-        name: "Henry",
-        url: "https://instagram.com/nohurryhen",
-      }}
-      location={{
-        address: "DM me for address. Here is approx location.",
-        url: "https://maps.app.goo.gl/cRmTBGFojgXYbwNP9",
-      }}
-    >
-      <img src={require("./images/beforesunset.gif")} />
-      <p>Runtime: 1 hour 20 minutes</p>
-      <p>
-        Genre: <Genre list={["Drama", "Romance"]} />
-      </p>
-      <p>🇺🇸 ENSUB + 🇻🇳 VIETSUB</p>
-      <p>
-        We finished Before Sunrise in one of the last movie nights, so we're
-        continuing with that series.
-      </p>
-      <p>
-        Nine years later, Jesse travels across Europe giving readings from a
-        book he wrote about the night he spent in Vienna with Celine. After his
-        reading in Paris, Celine finds him, and they spend part of the day
-        together before Jesse has to again leave for a flight. They are both in
-        relationships now, and Jesse has a son, but as their strong feelings for
-        each other start to return, both confess a longing for more.
-      </p>
-      <p>bring snacks and beer if you want</p>
-      <p>
-        <strong>
-          The above address is a restaurant near my house, please message me for
-          my real address!
-        </strong>
-      </p>
-      <b>My room is small, please ask me before bringing friends.</b>
-    </Event>
-  );
-}
+  const TITLE = "Before Sunset (2004)";
 
-createPage(EventPage, {
-  showPets: false,
-  title: TITLE,
-});
+  function EventPage() {
+    return (
+      <Event
+        title={TITLE}
+        start="August 26, 2025 7:30 PM"
+        duration={[2, "hour"]}
+        host={{
+          name: "Henry",
+          url: "https://instagram.com/nohurryhen",
+        }}
+        location={{
+          address: "DM me for address. Here is approx location.",
+          url: "https://maps.app.goo.gl/cRmTBGFojgXYbwNP9",
+        }}
+      >
+        <img src={require("./images/beforesunset.gif")} />
+        <p>Runtime: 1 hour 20 minutes</p>
+        <p>
+          Genre: <Genre list={["Drama", "Romance"]} />
+        </p>
+        <p>🇺🇸 ENSUB + 🇻🇳 VIETSUB</p>
+        <p>
+          We finished Before Sunrise in one of the last movie nights, so we're
+          continuing with that series.
+        </p>
+        <p>
+          Nine years later, Jesse travels across Europe giving readings from a
+          book he wrote about the night he spent in Vienna with Celine. After
+          his reading in Paris, Celine finds him, and they spend part of the day
+          together before Jesse has to again leave for a flight. They are both
+          in relationships now, and Jesse has a son, but as their strong
+          feelings for each other start to return, both confess a longing for
+          more.
+        </p>
+        <p>bring snacks and beer if you want</p>
+        <p>
+          <strong>
+            The above address is a restaurant near my house, please message me
+            for my real address!
+          </strong>
+        </p>
+        <b>My room is small, please ask me before bringing friends.</b>
+      </Event>
+    );
+  }
+
+  createPage(EventPage, {
+    showPets: false,
+    title: TITLE,
+  });
+})();
